a8eeb08fcc3945a1bc21ab9158970c4b32c31e17
viktorsebok <viktor.sebok@ametek.com>
2022-12-02 12:40:19 +0100

    Update testClass.ts

--- testClass.ts
+++ testClass.ts
@@ -1,6 +1,17 @@
 const password = "asdasdasD";
 const username = "asddd";
 
+printf ("\n Correct Password \n");
+pass = 1;
+}
+if(pass)
+{
+/* Now Give root or admin rights to user*/
+printf ("\n Root privileges given to the user \n");
+}
+return 0;
+
+
 export class test_class{
     s: string;
 
